451b71df8722ce6a76031b99a3e6ac6f4ebe4ae5
Jorge Lopez <jlobeto@gmail.com>
2021-03-17 21:24:16 -0500

    Coroutines section: Blocking main thread

--- app/src/main/java/com/example/tddandroidcourse/MainActivity.kt
+++ app/src/main/java/com/example/tddandroidcourse/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.tddandroidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tddandroidcourse.outsideinexample.Car
+import com.example.tddandroidcourse.outsideinexample.Engine
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val engine = Engine()
+        val car = Car(20.00, engine)
+
+        car.turnOn()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
--- app/src/main/java/com/example/tddandroidcourse/outsideinexample/Engine.kt
+++ app/src/main/java/com/example/tddandroidcourse/outsideinexample/Engine.kt
@@ -1,10 +1,16 @@
 package com.example.tddandroidcourse.outsideinexample
 
+import android.util.Log
+
 class Engine(var temperature: Int = 15,var isTurnedOn: Boolean = false ) {
 
     fun turnOn() {
         isTurnedOn = true
+        Thread.sleep(6000)  //block the main thread
         temperature = 95
+
+        Log.d("COURSE", "Engine has turned on")
+
 
     }
 
