b6e2dfae57ff9d9388555fb6bd83a19747c16fde
Jorge Lopez <jlobeto@gmail.com>
2021-03-17 22:05:20 -0500

    Coroutines section: Using Coroutines make a suspend function to show how to delay a method without blocking the main thread

--- app/src/main/java/com/example/tddandroidcourse/outsideinexample/Car.kt
+++ app/src/main/java/com/example/tddandroidcourse/outsideinexample/Car.kt
@@ -1,9 +1,15 @@
 package com.example.tddandroidcourse.outsideinexample
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class Car(var fuel: Double, val engine: Engine) {
     fun turnOn() {
         fuel -= 0.5
-        engine.turnOn()
-    }
 
+        CoroutineScope(Dispatchers.Main).launch {
+            engine.turnOn()
+        }
+    }
 }
--- app/src/main/java/com/example/tddandroidcourse/outsideinexample/Engine.kt
+++ app/src/main/java/com/example/tddandroidcourse/outsideinexample/Engine.kt
@@ -1,12 +1,13 @@
 package com.example.tddandroidcourse.outsideinexample
 
 import android.util.Log
+import kotlinx.coroutines.delay
 
 class Engine(var temperature: Int = 15,var isTurnedOn: Boolean = false ) {
 
-    fun turnOn() {
+    suspend fun turnOn() {
         isTurnedOn = true
-        Thread.sleep(6000)  //block the main thread
+        delay(6000)
         temperature = 95
 
         Log.d("COURSE", "Engine has turned on")
