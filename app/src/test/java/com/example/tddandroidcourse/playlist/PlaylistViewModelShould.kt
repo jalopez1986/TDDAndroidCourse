766576cf99ccdc8cd42b5f14e11712300647994a
Jorge Lopez <jlobeto@gmail.com>
2021-03-20 13:37:21 -0500

    Testing Error Cases

--- app/src/test/java/com/example/tddandroidcourse/playlist/PlaylistViewModelShould.kt
+++ app/src/test/java/com/example/tddandroidcourse/playlist/PlaylistViewModelShould.kt
@@ -11,13 +11,14 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-
+import java.lang.RuntimeException
 
 
 class PlaylistViewModelShould : BaseUnitTest() {
     private val repository: PlaylistRepository = mock()
     private val playlists = mock<List<Playlist>>()
     private val expected = Result.success(playlists)
+    private val exception = RuntimeException("Something went wrong")
 
     @Test
     fun getPlaylistsFromRepository() = runBlockingTest {
@@ -34,6 +35,21 @@ class PlaylistViewModelShould : BaseUnitTest() {
 
         assertThat(viewModel.playlists.getValueForTest()).isEqualTo(expected)
     }
+
+    @Test
+    fun emitErrorWhenReceiveError() {
+        runBlocking {
+            whenever(repository.getPlaylists()).thenReturn(
+                flow {
+                    emit(Result.failure<List<Playlist>>(exception))
+                }
+            )
+        }
+        val viewModel = PlaylistViewModel(repository)
+
+        assertThat(viewModel.playlists.getValueForTest()!!.exceptionOrNull()).isEqualTo(exception)
+    }
+
 
     private fun mockSuccessfulCase(): PlaylistViewModel {
         runBlocking {
