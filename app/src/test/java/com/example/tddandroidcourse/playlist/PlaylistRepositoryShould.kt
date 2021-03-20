7dc33f8d12f6b7013a2b645f701afbc8622b4c8f
Jorge Lopez <jlobeto@gmail.com>
2021-03-20 14:17:15 -0500

    Add PlaylistRepository error case test

--- app/src/test/java/com/example/tddandroidcourse/playlist/PlaylistRepositoryShould.kt
+++ app/src/test/java/com/example/tddandroidcourse/playlist/PlaylistRepositoryShould.kt
@@ -10,12 +10,12 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.lang.RuntimeException
 
 class PlaylistRepositoryShould : BaseUnitTest() {
-
     private val service: PlaylistService = mock()
     private val playlists = mock<List<Playlist>>()
-
+    private val exception = RuntimeException("Something went wrong")
 
     @Test
     fun getPlaylistsFromService() = runBlockingTest {
@@ -34,10 +34,29 @@ class PlaylistRepositoryShould : BaseUnitTest() {
         assertThat(repository.getPlaylists().first().getOrNull() ).isEqualTo(playlists)
     }
 
+    @Test
+    fun propagateErrors() = runBlockingTest {
+        val repository = mockFailureTest()
+
+        assertThat(repository.getPlaylists().first().exceptionOrNull()).isEqualTo(exception)
+    }
+
+
     private suspend fun mockSuccessfulCase(): PlaylistRepository {
         whenever(service.fetchPlaylists()).thenReturn(
             flow {
                 emit(Result.success(playlists))
+            }
+        )
+
+        val repository = PlaylistRepository(service)
+        return repository
+    }
+
+    private suspend fun mockFailureTest(): PlaylistRepository {
+        whenever(service.fetchPlaylists()).thenReturn(
+            flow {
+                emit(Result.failure<List<Playlist>>(exception))
             }
         )
 
