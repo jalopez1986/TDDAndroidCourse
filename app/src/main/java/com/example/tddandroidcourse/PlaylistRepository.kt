ac41bd16d58f91e8ef88dc90920d1b776a73cb8a
Jorge Lopez <jlobeto@gmail.com>
2021-03-20 11:43:31 -0500

    Red ViewModel Unit Test

---
+++ app/src/main/java/com/example/tddandroidcourse/PlaylistRepository.kt
@@ -0,0 +1,8 @@
+package com.example.tddandroidcourse
+
+class PlaylistRepository {
+    fun getPlaylists() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/test/java/com/example/tddandroidcourse/PlaylistViewModelShould.kt
@@ -0,0 +1,40 @@
+package com.example.tddandroidcourse
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.tddandroidcourse.utils.MainCoroutineScopeRule
+import com.example.tddandroidcourse.utils.getValueForTest
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+class PlaylistViewModelShould {
+    @get:Rule
+    var coroutinesRule = MainCoroutineScopeRule()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val viewModel: PlaylistViewModel
+    private val repository: PlaylistRepository = mock()
+
+    init {
+        viewModel = PlaylistViewModel()
+
+    }
+
+
+
+    @Test
+    fun getPlaylistsFromRepository() {
+        viewModel.playlists.getValueForTest()
+
+        verify(repository, times(1)).getPlaylists()
+
+
+
+    }
+}
